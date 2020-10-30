c83bdb821127f5ff8a071937ed748ed47bfd0415
Alexander Maricich <w33tmaricich@gmail.com>
2020-10-30 16:04:18 -0400

    feat(remarkable_watcher): uploading to remote server
    - added a gitignore to hide build files.
    - updated the readme to include new usage instructions.
    - removed the makefile since it holds sensitve information now.
    - monitored directory's contents are uploaded to a remote server on content
      change.

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ to the remarkable cloud and removed from the folder.
 ```
 pip3 install -r requirements.txt
 ```
+
+Make sure you have the following installed:
+- ssh
+- sshpass
+
+## Usage
+```
+usage: remarkable_watcher.py [-h] [--ip IP] [--username USERNAME]
+                             [--password PASSWORD] [--watchdir WATCHDIR]
+
+watch a directory and sync it with the remarkable locally.
+
+optional arguments:
+  -h, --help           show this help message and exit.
+  --ip IP              remarkable ip address.
+  --username USERNAME  remarkable username.
+  --password PASSWORD  remarkable password.
+  --watchdir WATCHDIR  the directory you want to sync.
+```
--- remarkable_watcher.py
+++ remarkable_watcher.py
@@ -3,6 +3,7 @@
 import argparse
 import time
 import logging as log
+import subprocess
 
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
@@ -11,9 +12,12 @@ from watchdog.observers import Observer
 log.basicConfig(level=log.DEBUG)
 
 # Arguments
-parser_description = "Watch a directory and sync with the remarkable"
+parser_description = "watch a directory and sync it with the remarkable locally."
 parser = argparse.ArgumentParser(description=parser_description)
-parser.add_argument("--watchdir", help="The directory you want to sync.")
+parser.add_argument("--ip", help="remarkable ip address.")
+parser.add_argument("--username", help="remarkable username.")
+parser.add_argument("--password", help="remarkable password.")
+parser.add_argument("--watchdir", help="the directory you want to sync.")
 args = parser.parse_args()
 
 # Configuration
@@ -25,16 +29,70 @@ case_sensitive = False
 watch_path = args.watchdir
 recursive = False
 
+remarkable_ip = args.ip
+remarkable_user = args.username
+remarkable_password = args.password
+
+def file_name_from_path(file_path):
+    split_path = file_path.split("/")
+    return split_path[-1]
+
+
+def run_cmd(cmd, success_msg, error_msg):
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+        log.info(success_msg)
+    except CalledProcessError:
+        log.error(error_msg)
+
+
+def upload_to_remarkable(file_path):
+    file_name = file_name_from_path(file_path)
+    cmd = "sshpass -p \"%s\" scp %s %s@%s:/tmp/%s" % (remarkable_password,
+          file_path, remarkable_user, remarkable_ip, file_name)
+    log.debug(cmd)
+    on_success = "Uploaded %s to %s." % (file_name, remarkable_ip)
+    on_fail = "Unable to upload %s to your remarkable at %s" % (file_name,
+              remarkable_ip)
+
+    run_cmd(cmd, on_success, on_fail)
+
+
+def remove_from_remarkable(file_path):
+    print("TODO: rm -f remote file")
+
 
 # Event handlers
 def on_created(event):
     log.info(f"{event.src_path} created.")
+    log.info(f"{event}.")
+    upload_to_remarkable(event.src_path)
+
+
+def on_deleted(event):
+    log.info(f"{event.src_path} deleted.")
+    log.info(f"{event}.")
+    remove_from_remarkable(event.src_path)
+
+
+def on_modified(event):
+    log.info(f"{event.src_path} modified.")
+    log.info(f"{event}.")
+    upload_to_remarkable(event.src_path)
+
+
+def on_moved(event):
+    log.info(f"{event.src_path} moved.")
+    log.info(f"{event}.")
 
 
 def setup_event_handler():
     event_handler = PatternMatchingEventHandler(patterns, ignore_patterns,
                     ignore_directories, case_sensitive)
     event_handler.on_created = on_created
+    event_handler.on_deleted = on_deleted
+    event_handler.on_modified = on_modified
+    event_handler.on_moved = on_moved
     return event_handler
 
 
